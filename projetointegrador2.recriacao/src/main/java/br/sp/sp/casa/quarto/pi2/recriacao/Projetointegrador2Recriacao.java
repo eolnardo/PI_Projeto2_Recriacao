ed2a405c484a2c1ca5a83f98d65e5ade437d9bd2
leonardo <116859982+eolnardo@users.noreply.github.com>
2023-07-17 19:10:01 -0300

    PI2_2 v0.0.1
    Criação do Projeto

---
+++ projetointegrador2.recriacao/src/main/java/br/sp/sp/casa/quarto/pi2/recriacao/Projetointegrador2Recriacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.sp.sp.casa.quarto.pi2.recriacao;
+
+/**
+ *
+ * @author eolna
+ */
+public class Projetointegrador2Recriacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
